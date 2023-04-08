c8c0760d6d9e7e4b5fba7d388f5300d6f7f46915
xiaocuiabc <110989697+xiaocuiabc@users.noreply.github.com>
2023-04-08 20:57:02 +0800

    Delete injector.py

--- injector.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-url = ("https://www.bilibili.com/video/BV1GJ411x7h7/?spm_id_from=333.337.search-card.all.click&vd_source=ffaf054ddbb73690604f3f8066f2e3ad")
-a=input("输入open注入辅助")
-if a=="open":
-    webbrowser.open(url)
-else:
-    print("sb字都能打错注入你妈")
